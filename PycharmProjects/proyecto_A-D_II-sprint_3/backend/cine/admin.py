f73fdb902ea143ea7372f9ddf67965a1fb678c98
JOSEALEJANDRODIAZALI88 <jose2341100@gmail.com>
2026-05-30 22:38:34 -0400

    corrige estructura del sprint 3

--- PycharmProjects/proyecto_A-D_II-sprint_3/backend/cine/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pelicula, Sala, Asiento, Funcion
-
-admin.site.register(Pelicula)
-admin.site.register(Sala)
-admin.site.register(Asiento)
-admin.site.register(Funcion)
